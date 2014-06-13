efcc313e056ec3002cdaef6232e2d2b9ec6d236a
Antoine Jackson <a.j.william26@gmail.com>
2014-06-13 13:20:43 +0200

    Removed plugins for step-0

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,25 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.phonegap.plugins.oauthio/www/dist/oauth.js",
-        "id": "com.phonegap.plugins.oauthio.OAuth",
-        "merges": [
-            "OAuth"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.phonegap.plugins.oauthio": "0.2.1",
-    "org.apache.cordova.inappbrowser": "0.5.1-dev"
-}
+{}
 // BOTTOM OF METADATA
 });
